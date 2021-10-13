7ecf3f36dc527c8a3a6b37b525369d58d0e52547
Nauyaca <tiburon-nahu@outlook.com>
2021-10-13 01:03:03 -0500

    conexion a la nuben

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const routerApi = require('./routes')
 const express = require('express')
 const app = express()
-const port = 3000
+const { config } = require('./config/index')
+
+const port = config.port
+
+const mongoose = require('mongoose')
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const DB_NAME = encodeURIComponent(config.dbName)
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`
+
+console.log('*************************')
+console.log(MONGO_URI)
+mongoose.connect(MONGO_URI,
+  {useNewUrlParser: true, useUnifiedTopology: true}
+).then(()=> console.log('Ya estamos conectados a THE HIVE')).catch(e=>console.log(e))
+
+
 
 routerApi(app)
 
